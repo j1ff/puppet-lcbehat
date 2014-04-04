c8fd79cfb7db210aad106289c3eef070f87b9200
Jeff Landfried <jlandfri@gmail.com>
2014-04-04 16:14:02 -0400

    add lcphp requirement

--- manifests/lcbehat.pp
+++ manifests/lcbehat.pp
@@ -1,4 +1,5 @@
 
+#lcphp repo - https://github.com/rbayliss/puppet-lcphp
 class lcbehat::lcbehat() {
 
   include composer
@@ -6,6 +7,8 @@ class lcbehat::lcbehat() {
   composer::exec { 'behat-install':
     cmd  => 'install',
     cwd  => '/vagrant/modules/lcbehat',
+    logoutput => true,
+    require => Class ["lcphp"],
   }
   package { 'default-jre':
     ensure => "installed",
